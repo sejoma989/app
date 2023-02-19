e413db0bd8a70bbdf10af15d9bff015387fcdc8f
Glitch (iron-spiced-holiday) <none>
2023-02-19 00:17:01 +0000

    🎰👂 Checkpoint ./server.js:54858107/1096

--- server.js
+++ server.js
@@ -18,42 +18,60 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// eg: https://curse-arrow.hyperdev.space/api/timestamp/2015-12-15
-// functions {"unix": <date.getTime()>, "utc" : <date.toUTCString()> }
-
-app.get("/api/timestamp", (req, res) => {
-  const date = new Date();
-  res.json({
-    unix: date.getTime(),
-    utc: date.toUTCString()
-  })
-});
-
 // your first API endpoint... 
-app.get("/api/timestamp/:date_str", function (req, res) {
-  const { date_str } = req.params;
-  
-  let date = new Date(date_str); 
-  
-  if(date.toString() === 'Invalid Date') {
-    // tries again by converting it from unix (which needs to be a number)
-    date = new Date(parseInt(date_str));
-  }
-  
-  if(date.toString() === 'Invalid Date') {
-    return res.json({
-      error: "Invalid Date" 
-    });
-  } else {
-    return res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString()
-    })
-  }
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
 });
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/api/timestamp", (req, res) => {
+//   const date = new Date();
+//   res.json({
+//     unix: date.getTime(),
+//     utc: date.toUTCString()
+//   })
+// });
+
+// your first API endpoint... 
+// app.get("/api/timestamp/:date_str", function (req, res) {
+//   const { date_str } = req.params;
+  
+//   let date = new Date(date_str); 
+  
+//   if(date.toString() === 'Invalid Date') {
+//     // tries again by converting it from unix (which needs to be a number)
+//     date = new Date(parseInt(date_str));
+//   }
+  
+//   if(date.toString() === 'Invalid Date') {
+//     return res.json({
+//       error: "Invalid Date" 
+//     });
+//   } else {
+//     return res.json({
+//       unix: date.getTime(),
+//       utc: date.toUTCString()
+//     })
+//   }
+// });
+
