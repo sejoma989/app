5af93fb61fdec552ffd999dcf2eb669022c65e7a
Glitch (iron-spiced-holiday) <none>
2023-02-18 20:22:00 -0500

    fea: timestamp microservice

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+const PORT = 8082;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -19,7 +20,17 @@ app.get("/", function (req, res) {
 });
 
 // example: https://curse-arrow.hyperdev.space/api/timestamp/2023-2-18
+// example2: https://curse-arrow.hyperdev.space/api/timestamp/1450137600000
+
 // functions {"unix": <date.getTime()>, "utc" : <date.toUTCString()> }
+
+app.get("/api/timestamp", (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+});
 
 // your first API endpoint... 
 // Create the timestamp endpoint
@@ -27,77 +38,31 @@ app.get("/api/timestamp/:date_str", function (req, res) {
 	
   // capturar parametro en url
   const { date_str } = req.params;
+
+  // convert the date
+  let date = new Date(date_str);
   
-  let date;
-  
-  // return when date invalid
-  if ( !date_str ){
-    date = new Date();
+  if ( date === null ) {
     return res.json({
-      unix: null, 
-      utc:'Invalid Date'
-    })
+   		unix: null, 
+  		utc : "Invalid Date" 
+		})
   }
   
-  // convert the date
-  date = new Date(date_str);
+  if(date.toString() === 'Invalid Date') {
+    // tries again by converting it from unix (which needs to be a number)
+    date = new Date(parseInt(date_str));
+  }
   
   return res.json({
     unix: date.getTime(), 
   	utc : date.toUTCString() 
 	})
-  
+})
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.get("/api/timestamp", (req, res) => {
-//   const date = new Date();
-//   res.json({
-//     unix: date.getTime(),
-//     utc: date.toUTCString()
-//   })
-// });
-
-// your first API endpoint... 
-// app.get("/api/timestamp/:date_str", function (req, res) {
-//   const { date_str } = req.params;
-  
-//   let date = new Date(date_str); 
-  
-//   if(date.toString() === 'Invalid Date') {
-//     // tries again by converting it from unix (which needs to be a number)
-//     date = new Date(parseInt(date_str));
-//   }
-  
-//   if(date.toString() === 'Invalid Date') {
-//     return res.json({
-//       error: "Invalid Date" 
-//     });
-//   } else {
-//     return res.json({
-//       unix: date.getTime(),
-//       utc: date.toUTCString()
-//     })
-//   }
-// });
-
